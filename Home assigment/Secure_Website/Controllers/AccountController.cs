21c44c83847fafc31b33405adb498c67a6838ba1
AdrianPortelli <adrian@keen-advertising.com>
2021-04-24 15:13:24 +0200

    First commit | added google login and applicaiton user

---
+++ Home assigment/Secure_Website/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Secure_Website.Models;
+
+namespace Secure_Website.Controllers
+{
+    [AllowAnonymous, Route("account")]
+    public class AccountController : Controller
+        
+        
+    {
+       
+
+    }
+}
---
+++ Home assigment/Secure_Website/Models/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Secure_Website.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+
+        
+    }
+}
